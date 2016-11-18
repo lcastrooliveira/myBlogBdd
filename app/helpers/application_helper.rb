e1f7e9ee1af00ed95060eb6e833658d1fdf39f60
Lucas de Castro Oliveira <lcastro.oliveira@gmail.com>
2016-11-18 11:09:43 -0200

    Stylize devise messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def devise_error_messages!
+    return '' if resource.errors.empty?
+    messages = resource.errors.full_messages.map {
+      |msg| content_tag(:li, msg)
+    }.join
+    html = <<-html
+    <div clas="alert alert-error alert-danger">
+      <a href='#' class='close' data-dismiss='alert' &#215;></a>
+      <%= content_tag :div, msg if msg.is_a?(String) %>
+    </div>
+    html
+    html.html_safe
+  end
 end
